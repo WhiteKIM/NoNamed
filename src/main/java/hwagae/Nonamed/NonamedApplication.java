a8139f9968dd72a8267b7b1606765c17a0856f6c
WhiteKIM <9rudrb40@naver.com>
2024-07-14 18:52:00 +0900

    init Project

---
+++ src/main/java/hwagae/Nonamed/NonamedApplication.java
@@ -0,0 +1,13 @@
+package hwagae.Nonamed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonamedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonamedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hwagae/Nonamed/NonamedApplicationTests.java
@@ -0,0 +1,13 @@
+package hwagae.Nonamed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonamedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
